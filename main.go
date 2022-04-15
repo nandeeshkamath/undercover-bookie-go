3cbd2aaeb541e4861477e252d238062cf992c103
Nandeesh Kamath <58938846+nandeeshkamath@users.noreply.github.com>
2022-04-15 18:42:15 +0530

    Improve error handling (#5)

--- main.go
+++ main.go
@@ -14,13 +14,31 @@ func main() {
 	regionSlug := os.Args[3]
 	regionCode := os.Args[4]
 	telegramChannelId := os.Args[5]
+	telegramDebugChannelId := "@nandeeshkamathdev"
 
-	response := clients.GetMovieSynopsis(eventId, regionCode, regionSlug)
+	if len(os.Args[6]) > 0 {
+		telegramDebugChannelId = os.Args[6]
+	}
+
+	response, error := clients.GetMovieSynopsis(eventId, regionCode, regionSlug)
+	if error != nil {
+		clients.SendMessage(telegramDebugChannelId, fmt.Sprint(error))
+	}
+
+	if len(response.BannerWidget.PageCta) == 0 {
+		clients.SendMessage(telegramDebugChannelId, "No booking found yet.")
+		return
+	}
 	bookingOpen := strings.Contains(response.BannerWidget.PageCta[0].Text, keyword)
 
 	if bookingOpen {
 		eventName := response.Meta.Event.EventName
-		bookingUrl := response.Seo.MetaProperties[7].Value
+		var bookingUrl string
+		if len(response.Seo.MetaProperties) > 0 {
+			bookingUrl = response.Seo.MetaProperties[7].Value
+		} else {
+			bookingUrl = "[ No booking url found ]"
+		}
 		message := fmt.Sprintf("%s is now ready to be booked at %s.\n%s", eventName, regionSlug, bookingUrl)
 		clients.SendMessage(telegramChannelId, message)
 	}
